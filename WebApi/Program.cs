378f798dce7c929689b00803cd61d58db72dd16c
thenix05 <tsering.tuenschitsang@outlook.com>
2026-05-13 15:26:12 +0200

    MongoDB integration

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Minimal API Version 1.0");
+
+app.MapGet("/check", () =>
+{
+    try
+    {
+        var connectionString =
+            "mongodb://gbs:geheim@localhost:27017";
+
+        var client = new MongoClient(connectionString);
+
+        var databases =
+            client.ListDatabaseNames().ToList();
+
+        return $"Zugriff auf MongoDB ok.\n\nDatenbanken:\n{string.Join("\n", databases)}";
+    }
+    catch (Exception ex)
+    {
+        return $"Fehler: {ex.Message}";
+    }
+});
 
 app.Run();
